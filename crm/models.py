8da9045d88a156537127dc0d00729b436bc539ca
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-11-16 10:08:51 +0100

    init repo with models and auth

---
+++ crm/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    TEAM_CHOICES = [("M", "management"), ("SA", "sales"), ("SU", "support")]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    team = models.CharField(max_length=50, choices=TEAM_CHOICES)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=250)
+    sales_contact = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class Contract(models.Model):
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    sales_contact = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField(default=False)
+    amount = models.FloatField()
+    payement_due = models.DateTimeField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class Event(models.Model):
+    client = client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    support_contact = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    event_status = models.ForeignKey(to=Contract, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField()
+    notes = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
---
+++ crm/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
