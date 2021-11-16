d515bbe1a9ebc0e7c63d658865ce60b46c55af63
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-11-16 15:56:53 +0100

    add update and retrieve client with user permissions

--- crm/permissions.py
+++ crm/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from .models import Client
 
 
 class IsSales(BasePermission):
@@ -19,3 +20,12 @@ class IsSupport(BasePermission):
         if request.user and request.user.groups.filter(name="support"):
             return True
         return False
+
+
+class IsSalesContact(BasePermission):
+
+    message = "Access not allowed ! Only the sales contact can access"
+
+    def has_object_permission(self, request, view, obj):
+        is_sales_contact = Client.objects.get(email=obj.email)
+        return is_sales_contact.sales_contact == request.user
--- crm/views.py
+++ crm/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from .serializers import UserSerializer, ClientSerializer
+from .serializers import ClientSerializer
 from .models import Client
-from .permissions import IsSales
+from .permissions import IsSales, IsSalesContact
 
 
 class ClientView(viewsets.ViewSet):
@@ -19,7 +19,23 @@ class ClientView(viewsets.ViewSet):
         serializer.save()
         return Response(serializer.data)
 
+    def retrieve(self, request, pk=None):
+        client = get_object_or_404(self.queryset, pk=pk)
+        self.check_object_permissions(self.request, client)
+        serializer = ClientSerializer(client)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        client = get_object_or_404(self.queryset, pk=pk)
+        self.check_object_permissions(self.request, client)
+        serializer = ClientSerializer(client, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
     def get_permissions(self):
         if self.action == "create" or self.action == "list":
             permission_classes = [IsSales]
+        elif self.action == "retrieve" or self.action == "update":
+            permission_classes = [IsAuthenticated, IsSalesContact]
         return [permission() for permission in permission_classes]
