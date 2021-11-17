1a53d7c234a6eee3247957d588024b278e147912
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-11-17 15:20:22 +0100

    in filters : change name to company_name

--- crm/filters.py
+++ crm/filters.py
@@ -4,9 +4,9 @@ from .models import Contract
 
 
 class ContractFilterSet(filters.FilterSet):
-    name = django_filters.CharFilter(field_name="client__company_name")
+    company_name = django_filters.CharFilter(field_name="client__company_name")
     email = django_filters.CharFilter(field_name="client__email")
 
     class Meta:
         model = Contract
-        fields = ["name", "email", "created_time", "amount"]
+        fields = ["company_name", "email", "created_time", "amount"]
