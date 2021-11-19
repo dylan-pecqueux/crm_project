2801e1fe320b15548a09a28ea4529d35e5215ad7
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-11-19 11:48:04 +0100

    delete user serializer

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,20 +1,5 @@
 from rest_framework import serializers
-from .models import User, Client, Contract, Event
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "first_name", "last_name", "email", "team", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        password = validated_data.pop("password", None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+from .models import Client, Contract, Event
 
 
 class ClientSerializer(serializers.ModelSerializer):
